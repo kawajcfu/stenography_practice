78447abede76a3a73a2153396baa4429a74feedc
Sota Kawasaki <sota.kawasaki.tafsm@gmail.com>
2025-04-30 19:15:54 +0900

    test

--- test.js
+++ test.js
@@ -65,6 +65,7 @@ function start() {
   let Textarea = document.getElementById('textarea');
   Textarea.style.display = "none";
   Genkou.style.display = "null";
+  Genkou.value = "null";
   let texttext = Textarea.value.trim();
 
   // 文字列を分割
